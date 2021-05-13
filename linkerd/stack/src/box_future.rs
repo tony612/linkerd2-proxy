ccdbab3194cda36b68cd5f40df2f83c4597af9a6
Eliza Weisman <eliza@buoyant.io>
2021-05-13 11:07:29 -0700

    stack: make BoxFuture layer always Clone
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/stack/src/box_future.rs
+++ linkerd/stack/src/box_future.rs
@@ -9,16 +9,19 @@ use std::{
 ///
 /// This is primarily useful when a service's `Future` type is not `Unpin` and
 /// must be boxed.
-#[derive(Clone, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct BoxFuture<T>(T);
+
+#[derive(Copy, Debug)]
+pub struct BoxFutureLayer<T>(std::marker::PhantomData<fn(T)>);
 
 impl<T> BoxFuture<T> {
     pub fn new(inner: T) -> Self {
         Self(inner)
     }
 
-    pub fn layer() -> impl tower::Layer<T, Service = Self> {
-        crate::layer::mk(Self::new)
+    pub fn layer() -> BoxFutureLayer<T> {
+        BoxFutureLayer(std::marker::PhantomData)
     }
 }
 
@@ -39,5 +42,20 @@ where
     #[inline]
     fn call(&mut self, req: R) -> Self::Future {
         Box::pin(self.0.call(req))
+    }
+}
+
+// === impl BoxFutureLayer ===
+
+impl<T> tower::Layer<T> for BoxFutureLayer<T> {
+    type Service = BoxFuture<T>;
+    fn layer(&self, svc: T) -> Self::Service {
+        BoxFuture(svc)
+    }
+}
+
+impl<T> Clone for BoxFutureLayer<T> {
+    fn clone(&self) -> Self {
+        Self(std::marker::PhantomData)
     }
 }
